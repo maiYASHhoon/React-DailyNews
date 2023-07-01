f130ab5b1ba073fb2c1fff5b5e186b8fa5738c72
maiYASHhoon <yash280801@gmail.com>
2023-07-01 14:20:26 +0530

    setting up redux

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ import Router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/app.css";
 
+import {Provider} from "react-redux";
+import {store} from "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+root.render(
+    <Provider store={store}>
+        <Router />
+    </Provider>
+);
